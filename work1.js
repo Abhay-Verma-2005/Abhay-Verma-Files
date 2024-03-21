85580d8c80e0f24ffb85d5a6b2e664d4bbeeeffa
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-03-21 18:49:31 +0530

    FILES

---
+++ work1.js
@@ -0,0 +1,12 @@
+var m=10
+let q=10
+const r=20
+q="hlo"
+console.log(typeof m)
+console.log(typeof r)
+console.log(typeof q)
+let fname ='Abhay'
+let lname ='Verma'
+console.log("f_name:"+fname)
+console.log("l_name:"+lname)
+console.log(`hi, ${fname} ${lname}!`)
