8dac2b4e302c6d8c05d9ff79803d2b1739a0c11d
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-03-23 16:31:06 +0530

    files

---
+++ work3.js
@@ -0,0 +1,15 @@
+//function
+function funcName(name){
+    let t="hi " + name;
+    console.log(t);
+    console.log(typeof t)
+}
+funcName("rahul")
+funcName(1)
+funcName(true)
+
+prompt
+let pr=prompt("Enter your name:");
+
+//Alert method
+// alert("THIS IS GIVE A MESSAGE !")
