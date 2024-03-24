adb031b75f1454f41c5c3e1abb75e290c51092da
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-03-24 18:47:43 +0530

    file

---
+++ work4.js
@@ -0,0 +1,21 @@
+
+//Add element in list
+let l1=['red','blue'];
+l1[2]=4
+l1[4]=55
+console.log(l1.length);
+
+
+//Comparision operator { == or != and === or !== and <,> >=,<=}
+let x='10'
+let y=10
+console.log(x==y);
+console.log(x===y); // also check the datatype of that variable
+
+// Logical operator ( && , || , !)
+console.log(x&&y);
+
+// Logical operators with non boolean vallue (falsey)--> number ,none,undefined
+//   and  (truthy)--> string
+
+//Bitwise(& , | , ^ , << , >> )
