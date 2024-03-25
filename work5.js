15e714204689b818a030dce99b9bda7182904b88
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-03-25 08:56:19 +0530

    files

---
+++ work5.js
@@ -0,0 +1,27 @@
+//Function -> function defination
+function maxArray(nameOfArray){
+        let max = 0;
+        for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] >= max) {
+        max = arr1[i];}
+        }
+        console.log(max);        
+        }
+        
+        //taken a sample array like:
+        let arr1 = [-1, 31, 51, 6, 7];
+        maxArray(arr1) // calling the function
+        //desired output: 51
+
+//Object
+function creatReactangle(length , breadth)
+{
+    return{length,
+        breadth,
+        area(){
+                console.log("Area: ", length*breadth)
+        }
+    }
+}
+const rect1=creatReactangle(5,11);
+rect1.area();4
