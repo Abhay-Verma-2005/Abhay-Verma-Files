83f103a9a771e527663c1d711f9c4df9e99d9534
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-03-31 18:00:42 +0530

    files

---
+++ work10.js
@@ -0,0 +1,21 @@
+function listele(name){
+    console.log("hi"+name)
+}
+l=["a","i","e"];
+for(let i of l){
+    console.log(i);
+}
+for(let i=0 ; i < l.length ; i++){
+    console.log(l[i] +" "+ i);
+}
+
+
+//MAX ELEMENT IN ARRAY
+a=[1,12,3,4,55,7];
+let x=a[0];
+for(let i of a){
+    
+    if (x<i){
+        x=i;
+    }
+}
