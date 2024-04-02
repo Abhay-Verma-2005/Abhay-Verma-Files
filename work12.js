bd9d552aaf321169646e28b98c05fddc1dc6113a
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-04-02 18:48:31 +0530

    files

---
+++ work12.js
@@ -0,0 +1,20 @@
+
+// Factorial
+let u = 6;
+let fact=1;
+while (u>0) {
+    fact=fact*u;
+    u=u-1;
+}
+console.log(fact);
+
+// Positive or  negative or zero
+let g=1;
+if (g>0)
+    console.log("positive")
+
+else if (g==0)
+    console.log("zero")
+
+else
+    console.log("negative")
