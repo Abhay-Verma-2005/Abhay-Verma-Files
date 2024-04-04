9e740a855ef5952b0ae4cb5523d7847efda1c15d
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-04-04 18:52:16 +0530

    files

---
+++ work14.js
@@ -0,0 +1,15 @@
+//Count vowels in a string
+
+function Vowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i].toLowerCase())) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const b = "Anant";
+console.log("Vowels:",Vowels(b));
